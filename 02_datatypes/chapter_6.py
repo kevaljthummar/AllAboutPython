0e48d0643f40d436db7703c21465779eb6af9baf
Keval Thummar <59870476+kevaljthummar@users.noreply.github.com>
2025-08-07 10:39:28 +0530

    update minor change

--- 02_datatypes/chapter_6.py
+++ 02_datatypes/chapter_6.py
@@ -6,7 +6,7 @@ print(f"Order for {customer_name} : {chai_type} please !")
 chai_description = "Aromatic and Bold"
 print(f"First word: {chai_description[:8]}")
 print(f"Last word: {chai_description[12:]}")
-print(f"Last word: {chai_description[::-1]}")
+print(f"Reverse Sentance: {chai_description[::-1]}")
 
 label_text = "Chai Spécial"
 ecoded_label = label_text.encode("utf-8")
